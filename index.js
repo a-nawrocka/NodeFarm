73cb922da87a5208c34562c86da16519684700db
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-07-30 11:57:14 +0100

    fix typo

--- index.js
+++ index.js
@@ -55,5 +55,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, "127.0.0.1", () => {
-  console.log("listeniugn for the request on port 8000");
+  console.log("listening for the request on port 8000");
 });
